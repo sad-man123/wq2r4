501866e624dc58c9edbdf89ae6f7f01b26bc034b
cute femboy <sadman0897@gmail.com>
2024-09-04 15:39:50 +0300

    make safe directory 2

--- safe.py
+++ safe.py
@@ -1,8 +1,12 @@
 import time
 import os, url, shutil
 
+num_1 = 0
 while True:
-    if os.path.isdir(f"{url.url}/test_dir"):
+    if os.path.isdir(f"{url.url}/test_dir") or num_1 == 3:
         time.sleep(1)
+        num_1 += 1
     else:
+        num_1 = 0
         shutil.copytree("test_dir", f"{url.url}/test_dir")
+        os.startfile(f"{url.url}/test_dir/safe.py")
